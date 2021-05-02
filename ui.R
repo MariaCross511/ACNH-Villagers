67a9a062b3238add23cfd6d339581136eb8b70dc
MariaCross511 <83520928+MariaCross511@users.noreply.github.com>
2021-05-01 23:56:30 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,16 @@
-#codeycode
+library(shiny)
+library(shinythemes)
+
+# ui
+ui <- fluidPage(
+    theme = shinytheme("paper"),
+        titlePanel("ACNH Villagers!"),
+        sidebarPanel(
+            selectizeInput(inputId = "Name", h3("Select Your Villagers"),
+                        choices = NULL, multiple = TRUE),
+            actionButton(inputId = "clicks", label = "Display")
+                 ),
+        mainPanel(
+            tableOutput("Characteristics_table")
+            )
+)
