fc96ab9c6b83f76b7b8421fb1a7862487f13c66a
MariaCross511 <83520928+MariaCross511@users.noreply.github.com>
2021-05-01 23:56:59 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,1 +1,10 @@
-#codecodecode
+# server
+#source("global.R")
+
+server <- function(input, output) {
+
+        observeEvent(input$clicks, {
+            output$Characteristics_table = renderTable(Villagers)
+        })
+    
+}
